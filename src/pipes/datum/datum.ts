c0941cded2c0048c651ef9d4e7d890b7bb1426f1
sfrimont <sfrimont@users.noreply.github.com>
2019-01-23 11:12:24 +0100

    Update datum.ts

--- src/pipes/datum/datum.ts
+++ src/pipes/datum/datum.ts
@@ -13,10 +13,18 @@ export class DatumPipe implements PipeTransform {
    * Konvertiert ein Datum in einen String
    */
   transform(value: string): string {
-
-    if (value != undefined)
-                        return new Date(value).toLocaleDateString();
-                    else
+    let wochentag: string;
+    if (value != undefined) {
+                        wochentag = new Date(value).getDay()
+                        if (wochentag==0) wochentag="So ,");
+                        if (wochentag==1) wochentag="Mo ,");
+                        if (wochentag==2) wochentag="Di ,");
+                        if (wochentag==3) wochentag="Mi ,");
+                        if (wochentag==4) wochentag="Do ,");
+                        if (wochentag==5) wochentag="Fr ,");
+                        if (wochentag==6) wochentag="Sa ,");
+                        return wochentag + new Date(value).toLocaleDateString();
+    } else
                         return null;
   }
 }
